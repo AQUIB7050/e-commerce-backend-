c58f2900b6dbff226e1099ed4dad0370fb5192ec
Aquib Ahmed Ansari <54367316+AQUIB7050@users.noreply.github.com>
2024-10-19 18:45:50 +0530

    entities added

---
+++ src/main/java/com/ecommerce/model/Rating.java
@@ -0,0 +1,34 @@
+package com.ecommerce.model;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Rating {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne
+	@JoinColumn(name="product_id", nullable=false)
+	private User user;
+	
+	@JsonIgnore
+	@ManyToOne
+	@JoinColumn(name="product_id", nullable=false)
+	private Product product;
+	
+	private double rating;
+	
+	private LocalDateTime createdAt;
+
+}
